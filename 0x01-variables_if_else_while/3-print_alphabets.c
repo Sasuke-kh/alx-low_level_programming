d834b7fecda595ff01d33b4c3c4dccbccc741cd0
omar-kh <omerkhalid56682@gmail.com>
2023-09-13 16:11:12 +0200

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints "a...z"
+ *
+ * Return: 0 (success)
+ */
+
+	int main(void)
+{
+	for (char x = 'a' ; x <= 'z'; x++)
+		putchar(x);
+	return (0);
+}

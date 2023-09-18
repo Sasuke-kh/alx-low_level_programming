0f32414cd9d344a4f474cdf5add87ea5ea42553b
omar-kh <omerkhalid56682@gmail.com>
2023-09-18 21:21:50 +0200

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print 0 to 9
+ *
+ * Return: 1 is uppercase 0 is lowercase
+ */
+
+void print_numbers(void)
+{
+	int digit = '0';
+
+	while (digit >= 0 && digit <= 9)
+		_putchar("%d", digit);
+	_putchar('\n')
+}

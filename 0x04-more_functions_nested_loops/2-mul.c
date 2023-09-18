f05600efdc1d4e4ae2fa8add532634f038040137
omar-kh <omerkhalid56682@gmail.com>
2023-09-18 21:03:44 +0200

    update

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Returns 1 if c is uppercase
+ *          Returns 0 otherwise
+ *
+ * @c: alphabet
+ *
+ * Return: always 0
+ */
+
+int mul(int a, int b)
+{
+	_putchar(a * b);
+
+	return (0);
+}

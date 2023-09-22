e2c8c11fef731ed85b0e1c66bc7a14c7c3d8c772
omar-kh <omerkhalid56682@gmail.com>
2023-09-22 12:03:18 +0200

    update

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies src
+ *
+ * @dest: parameter
+ * @src: parameter
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int y = -1;
+
+	do {
+		y++;
+		dest[y] = src[y];
+	} while (src[y] != '\0');
+
+	return (dest);
+}

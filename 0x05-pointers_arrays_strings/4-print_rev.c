9ba84574fc3c62224a61950dddd2657aa186f493
omar-kh <omerkhalid56682@gmail.com>
2023-09-22 03:05:16 +0200

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - revers the string
+ *
+ * @n: parameter
+ */
+
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (a[x])
+		x++;
+
+	while (x--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}

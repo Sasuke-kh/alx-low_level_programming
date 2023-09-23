46babaafac3c836331d33e998486447a3cdf3636
omar-kh <omerkhalid56682@gmail.com>
2023-09-23 03:56:20 +0200

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * string_toupper - compares two strings
+ *
+ * @s2: type char
+ * @s1: char type
+ *
+ * Return: value
+ */
+
+char *string_toupper(char *)
+{
+	char *str;
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] >= 97 && str [x] <= 122)
+		{
+			str[x] = str [x] - 32;
+		}
+	}
+	return (str);
+}

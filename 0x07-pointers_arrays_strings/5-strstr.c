ab6807daf73c71785fd6884df3c1ac5e7634c2a0
omar-kh <omerkhalid56682@gmail.com>
2023-09-27 19:17:24 +0200

    update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strstr - compares two strings
+ * @haystack: type char
+ * @needle: type char
+ * Return: s which is
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+
+}

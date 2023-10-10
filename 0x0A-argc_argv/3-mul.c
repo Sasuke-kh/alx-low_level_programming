68c1b8a457687dd82f128b92419ba392d93b0c44
omar-kh <omerkhalid56682@gmail.com>
2023-10-10 10:15:30 +0200

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - main function
@@ -18,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		printf("error");
+		printf("Error\n");
 		return (1);
 	}
 	return (0);

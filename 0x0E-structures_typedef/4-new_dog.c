eff7e8b0d4decbb60ebf89e2483d6977c606c5b1
omar-kh <omerkhalid56682@gmail.com>
2023-10-13 15:52:09 +0200

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,52 +1,49 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include "dog.h"
 
 /**
  * new_dog - Creates a new dog_t instance.
- * @name: Pointer to the name string.
- * @age: Age of the dog.
- * @owner: Pointer to the owner string.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * Return: Pointer to the newly created dog_t instance
+ * Return: Pointer to the newly created dog_t instance, or NULL if failed.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	size_t name_length, owner_length;
+	unsigned int name_length = 0, owner_length = 0, i;
 	dog_t *new_dog;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
+	/* Calculate the lengths of name and owner */
+	while (name[name_length++])
+		;
+	while (owner[owner_length++])
+		;
 
-	new_dog = malloc(sizeof(dog_t));
+	/* Allocate memory for the new dog_t instance */
+	new_dog = (dog_t *) malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_length = strlen(name);
-	owner_length = strlen(owner);
+	/* Allocate memory for the name and owner strings */
+	new_dog->name = (char *) malloc(name_length * sizeof(char));
+	new_dog->owner = (char *) malloc(owner_length * sizeof(char));
 
-	new_dog->name = malloc((name_length + 1) * sizeof(char));
-	if (new_dog->name == NULL)
+	if (new_dog->name == NULL || new_dog->owner == NULL)
 	{
+		/* Memory allocation failed, free previously allocated memory and return NULL */
 		free(new_dog);
-		return (NULL);
-	}
-
-	new_dog->owner = malloc((owner_length + 1) * sizeof(char));
-	if (new_dog->owner == NULL)
-	{
 		free(new_dog->name);
-		free(new_dog);
 		return (NULL);
 	}
 
-	strncpy(new_dog->name, name, name_length);
-	new_dog->name[name_length] = '\0';
+	/* Copy the name and owner strings to the new dog */
+	for (i = 0; i < name_length; i++)
+		new_dog->name[i] = name[i];
 
-	strncpy(new_dog->owner, owner, owner_length);
-	new_dog->owner[owner_length] = '\0';
+	for (i = 0; i < owner_length; i++)
+		new_dog->owner[i] = owner[i];
 
+	/* Set the age of the new dog */
 	new_dog->age = age;
 
 	return (new_dog);

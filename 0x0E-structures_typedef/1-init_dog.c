9ccb5112681b02211bb6da7ba6843c8b5e9393d7
omar-kh <omerkhalid56682@gmail.com>
2023-10-13 08:52:23 +0200

    update

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -3,15 +3,20 @@
 #include "dog.h"
 
 /**
- *
+ * int_dog - dog info
+ * @d: the dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: nothing
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
-		d->name = name
-		d->age = age
-		d->owner = owner
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }

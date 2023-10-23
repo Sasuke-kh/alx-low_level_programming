f08eb6f620fc5fb28b0798c251f932877837cdb8
omar-kh <omerkhalid56682@gmail.com>
2023-10-23 15:30:22 +0200

    upadte

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node_end - add a new node at the beg
+ * @head: node header
+ * @str: string to duplicate
+ * Return: address of new element
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *newnode = malloc(sizeof(list_t));
+
+	newnode->str = strdup(str);
+	newnode->len = strlen(str);
+	newnode->next = NULL;
+
+	*head = newnode;
+
+	if (*head == NULL)
+		return (NULL);
+	else
+		return (*head);
+}

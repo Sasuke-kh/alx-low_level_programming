394e19f811f4db53b21c77fe40401dc5b8c1ad18
omar-kh <omerkhalid56682@gmail.com>
2023-10-23 15:00:24 +0200

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,12 +3,16 @@
 #include <string.h>
 
 /**
- *
+ * add_node - add a new node at the beg
+ * @head: node header
+ * @str: string to duplicate
+ * Return: address of new element
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newnode = malloc(sizeof(list_t));
+
 	newnode->str = strdup(str);
 	newnode->len = strlen(str);
 	newnode->next = NULL;
@@ -16,5 +20,8 @@ list_t *add_node(list_t **head, const char *str)
 	newnode->next = *head;
 	*head = newnode;
 
-	return(*head);
+	if (newnode == NULL)
+		return (NULL);
+	else
+		return(*head);
 }

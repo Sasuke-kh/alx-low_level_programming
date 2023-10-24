9a6867d7a4d4ca0225b8235d92f0b603ecdd7a07
omar-kh <omerkhalid56682@gmail.com>
2023-10-24 16:14:50 +0200

    upadte

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -9,14 +9,15 @@ main:
     push rbp
     mov rbp, rsp
 
-    sub rsp, 8  ; Adjust stack pointer for alignment
+    sub rsp, 8 ; Adjust stack pointer for alignment
 
     lea rdi, [rel message]
-    xor eax, eax  ; Clear eax register
+    xor eax, eax ; Clear eax register
     call printf
 
-    add rsp, 8  ; Restore stack pointer
-    mov eax, 0  ; Return 0
+    add rsp, 8 ; Restore stack pointer
+
+    mov eax, 0 ; Return 0
 
     mov rsp, rbp
     pop rbp

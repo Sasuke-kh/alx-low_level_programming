2bd6e43d8f5353bf655ed8fff1701efb206854fb
omar-kh <omerkhalid56682@gmail.com>
2023-10-24 15:58:37 +0200

    upadte

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+section .data
+    message db 'Hello, Holberton', 0
+
+section .text
+    extern printf
+
+global main
+main:
+    push rbp
+    mov rbp, rsp
+
+    sub rsp, 8  ; Adjust stack pointer for alignment
+
+    lea rdi, [rel message]
+    xor eax, eax  ; Clear eax register
+    call printf
+
+    add rsp, 8  ; Restore stack pointer
+    mov eax, 0  ; Return 0
+
+    mov rsp, rbp
+    pop rbp
+    ret

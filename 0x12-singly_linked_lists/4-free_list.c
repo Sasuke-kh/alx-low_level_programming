8f6df8ed211e73530ebbfeda07a78e88c8b06e25
omar-kh <omerkhalid56682@gmail.com>
2023-10-24 12:27:00 +0200

    upadte

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,14 +8,14 @@
 
 void free_list(list_t *head)
 {
-	list_t *temp;
 	list_t *ptr = head;
+	list_t *temp;
 
-	while(ptr != NULL)
+	while (ptr != NULL)
 	{
 		temp = ptr->next;
+		free(ptr->str);
 		free(ptr);
 		ptr = temp;
 	}
-	head = NULL;
 }

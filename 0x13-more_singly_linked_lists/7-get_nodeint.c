d0af988d05e55dc782711580ed0b9fc2c3ec64c1
omar-kh <omerkhalid56682@gmail.com>
2023-10-25 14:28:52 +0200

    upadte

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
- *
+ * get_nodeint_at_index - get node at the index
+ * @head: node header
+ * @index: givn index
+ * Return: node address
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
@@ -10,9 +13,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (counter < index)
 	{
-		if(head == NULL)
+		if (head == NULL)
 			return (NULL);
-		
+
 		head = head->next;
 		counter++;
 	}

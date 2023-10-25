184fa192d2beaf2932b075d36466de2106ec8c82
omar-kh <omerkhalid56682@gmail.com>
2023-10-25 20:50:31 +0200

    upadte

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 	listint_t *ptr = *head;
 	listint_t *temp;
 
+	if (head == NULL || *head == NULL)
+		return;
 	while (ptr != NULL)
 	{
 		temp = ptr->next;
 		free(ptr);
 		ptr = temp;
 	}
-	**head = NULL;
+	*head = NULL;
 }

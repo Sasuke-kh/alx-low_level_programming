6ed4842464282cfd7c005bd3a2be526a9eb76808
[omar khalid] <[omerkhalid56682@gmail.com]>
2023-10-31 17:37:05 +0200

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	newfile = open(filename, O_CREAT | O_WRONLY);
 	if (newfile == -1)
 		return (-1);
-	for(count = 0; text_content[count] != '\0';)
+	for (count = 0; text_content[count] != '\0';)
 	{
 		count++;
 	}

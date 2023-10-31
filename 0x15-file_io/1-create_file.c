2617271bd8f2bf6d0698c20311cc4a12ddd9293a
[omar khalid] <[omerkhalid56682@gmail.com]>
2023-11-01 00:27:23 +0200

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,8 @@ int create_file(const char *filename, char *text_content)
 	ssize_t checkw;
 
 	if (text_content == NULL)
-	newfile = open(filename, O_CREAT | O_WRONLY);
+		text_content = "";
+	newfile = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (newfile == -1)
 		return (-1);
 	for (count = 0; text_content[count] != '\0';)

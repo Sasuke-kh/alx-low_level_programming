ec02c3d78c0961e15ad509d19ebbc36718e0bf12
Sasuke-kh <omerkhalid56682@gmail.com>
2023-12-08 22:21:47 +0200

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,15 +10,21 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
-	for (count = 0; h->next != NULL; count++)
+	if (h->next)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		for (count = 0; h->next != NULL; count++)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+		}
 	}
-	for (count++; h->prev != NULL;)
+	if (h->prev)
 	{
-		printf("%d\n", h->n);
-		h = h->prev;
+		for (count++; h->prev != NULL;)
+		{
+			printf("%d\n", h->n);
+			h = h->prev;
+		}
 	}
 	return (count);
 }
